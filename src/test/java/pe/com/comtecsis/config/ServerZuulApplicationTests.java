7df339dce4ef9dce3bb5ace1279e726dbc3a4e72
Elvis Perez <elperez@synopsis.ws>
2020-01-25 18:51:32 -0500

    Commit Inicial

---
+++ src/test/java/pe/com/comtecsis/config/ServerZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.comtecsis.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
